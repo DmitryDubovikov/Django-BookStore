ccca0d3a675aafd20760630741c02ea0719e58c2
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-04-18 14:27:42 -0300

    pages app and tests

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import home_page_view
 
-urlpatterns = [path("", home_page_view, name="home")]
+from .views import HomePageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+]
