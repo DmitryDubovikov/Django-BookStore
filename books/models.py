655cd0afb9c065d2b42f512b802a1a1cfc3be5f9
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-04-19 15:05:24 -0300

    image uploads

--- books/models.py
+++ books/models.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid  # new
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
@@ -9,6 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     def __str__(self):
         return self.title
@@ -17,7 +18,7 @@ class Book(models.Model):
         return reverse("book_detail", args=[str(self.id)])
 
 
-class Review(models.Model):  # new
+class Review(models.Model):
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
